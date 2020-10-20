5467c801801bab7c63c79cb9f997688aa5281993
wisnubayuaji <wisnubayu34@gmail.com>
2020-10-20 19:36:34 +0700

    Add files via upload

---
+++ if2-10116430-Latihan1-Biodata/src/App.java
@@ -0,0 +1,18 @@
+/**
+ * @author
+ * Nim  : 10116430
+ * Nama : Wisnu Bayu Aji
+ * Kelas: IF-2
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("BIODATA" + "\n" + "------------------------");
+        System.out.println("NAMA\t\t: Wisnu Bayu Aji");
+        System.out.println("ALAMAT\t\t: Komplek Bukit Permata");
+        System.out.println("TEMPAT LAHIR\t: Cimahi");
+        System.out.println("TANGGAL LAHIR\t: 3 Februari 1999");
+        System.out.println("------------------------");
+        System.out.println("MAKES\t\t: Mie Ayam");
+        System.out.println("MIKES\t\t: Es Klapa");
+    }
+}
